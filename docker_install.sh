257e578e6cc91339095aa96c133d54208cf696bc
vakiko <victoria.c6140@gmail.com>
2022-12-31 17:18:20 -0600

    add dockerfile + scripts

---
+++ docker_install.sh
@@ -0,0 +1,8 @@
+virtualenv --python=/usr/bin/python3.8 venv
+source venv/bin/activate
+pip install -r requirements.txt -t venv/lib/python3.8/site-packages
+cd venv/lib/python3.8/site-packages
+touch __init__.py
+cd ../
+mv site-packages libs
+zip -r9 deployment_package.zip .
---
+++ runner.sh
@@ -0,0 +1,11 @@
+container_name=aws_container
+docker_image=2dbbf98ac650
+docker run -td --name=$container_name $docker_image
+docker cp ./requirements.txt $container_name:/
+
+docker exec -i $container_name /bin/bash  < ./docker_install.sh
+docker cp $container_name:/venv/lib/python3.8/deployment_package.zip deployment_package.zip # copied in the root dir
+docker stop $container_name #stops container
+docker rm $container_name
+
+zip -g deployment_package.zip lambda_function.py
